b0bc65ba4ccaa457bdf66334376bdd8afe9e3f7c
agasconn <alex.gascon1999@gmail.com>
2025-05-13 10:56:38 +0200

    climb fix

--- components/elevation_chart.py
+++ components/elevation_chart.py
@@ -3,19 +3,19 @@ import matplotlib.pyplot as plt
 
 def get_color(grade):
     if grade >= 18:
-        return "#8B0000"  # Deep Red
+        return "#8B0000"
     elif grade >= 10:
-        return "#FF8C00"  # Orange
+        return "#FF8C00"
     elif grade >= 2:
-        return "#FFFF00"  # Yellow
+        return "#FFFF00"
     elif grade >= 0:
-        return "#ADFF2F"  # Light Green
+        return "#ADFF2F"
     elif grade >= -2:
-        return "#ADD8E6"  # Light Blue
+        return "#ADD8E6"
     elif grade >= -10:
-        return "#0000FF"  # Blue
+        return "#0000FF"
     else:
-        return "#00008B"  # Dark Blue
+        return "#00008B"
 
 def apply_slope_smoothing(df, target_meters=300):
     meters_per_point = df["distance"].iloc[-1] / len(df)
